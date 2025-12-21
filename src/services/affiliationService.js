ec3bcd634e2908e540d58fc931f253f88fb21469
Pritty Biswas <prittybiswas@gmail.com>
2025-12-21 11:46:09 +0530

    Configure app routes, services, and core project setup

--- src/services/affiliationService.js
+++ src/services/affiliationService.js
@@ -2,3 +2,6 @@ import axios from "../config/axios";
 
 export const getMyAffiliations = () =>
   axios.get("/affiliations/me");
+
+export const getHREmployees = () =>
+  axios.get("/affiliations/hr");
